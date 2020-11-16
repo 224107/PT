3e3e79ae5023b1565f6cf221768b4d41d4adf38e
Mateusz Stolarczyk <224102@edu.p.lodz.pl>
2020-11-16 21:38:24 +0100

    Cleaning

--- Shop/Data/Classes/ProductsCatalog.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections.Generic;
-
-namespace Data
-{
-    public class ProductsCatalog
-    {
-        public List<Product> Products { get; set; } = new List<Product>();
-    }
-}

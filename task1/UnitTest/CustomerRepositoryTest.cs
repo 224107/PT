a8e873546886e1662b8b524f2c45e2c50f9c85b7
Oskar Zaleski <224107@edu.p.lodz.pl>
2020-11-23 20:38:09 +0100

    Repository Tests changed

--- task1/UnitTest/CustomerRepositoryTest.cs
+++ task1/UnitTest/CustomerRepositoryTest.cs
@@ -3,67 +3,70 @@ using Data.Interfaces;
 using Logic;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
+using System.Linq;
 
-namespace UnitTests
+namespace DataUnitTests
 {
     [TestClass]
     public class CustomerRepositoryTest
     {
         private ICustomer repository;
         private DataContext data;
+        private IDataGenerator generator;
 
         [TestInitialize]
         public void Initialize()
         {
             data = new DataContext();
             repository = new CustomerRepository(data);
+            generator = new FixedExampleData();
+            //generator = new RandomExampleData();
+            generator.GenerateData(data);
         }
 
         [TestMethod]
         public void AddCustomerTest()
         {
-            Customer customer = new Customer(4, "Matt", "Knox");
-            Assert.AreEqual(3, repository.GetAllCustomers().Count);
-            repository.AddCustomer(customer);
+            Customer customer = new Customer(101, "Matt", "Knox");
             Assert.AreEqual(4, repository.GetAllCustomers().Count);
+            repository.AddCustomer(customer);
+            Assert.AreEqual(5, repository.GetAllCustomers().Count);
         }
 
         [TestMethod]
         public void GetCustomerByIdTest()
         {
-            Customer customer = new Customer(4, "Matt", "Knox");
+            Customer customer = new Customer(101, "Matt", "Knox");
             repository.AddCustomer(customer);
-            Customer customerById = repository.GetCustomerById(4);
+            Customer customerById = repository.GetCustomerById(101);
             Assert.AreEqual(customer, customerById);
         }
 
         [TestMethod]
         public void UpdateCustomerTest()
         {
-            Customer customer = repository.GetCustomerById(1);
-            Customer customerUpdated = new Customer(1, "Matt", "Knox");
-            Assert.AreEqual(customer, repository.GetCustomerById(1));
+            Customer customer = repository.GetAllCustomers().First();
+            Customer customerUpdated = new Customer(customer.Id, "Megan", "Fox");
+            repository.AddCustomer(customer);
+            Assert.AreEqual(customer, repository.GetCustomerById(customer.Id));
             repository.UpdateCustomer(customerUpdated);
-            Assert.AreEqual(customer, repository.GetCustomerById(1));
-            Assert.AreEqual(customerUpdated.FirstName, repository.GetCustomerById(1).FirstName);
-            Assert.AreEqual(customerUpdated.LastName, repository.GetCustomerById(1).LastName);
+            Assert.AreEqual(customerUpdated.FirstName, repository.GetCustomerById(customer.Id).FirstName);
+            Assert.AreEqual(customerUpdated.LastName, repository.GetCustomerById(customer.Id).LastName);
         }
 
         [TestMethod]
         public void DeleteCustomerTest()
         {
+            Assert.AreEqual(4, repository.GetAllCustomers().Count);
+            Customer customer = repository.GetAllCustomers().First();
+            repository.DeleteCustomer(customer.Id);
             Assert.AreEqual(3, repository.GetAllCustomers().Count);
-            repository.DeleteCustomer(1);
-            Assert.AreEqual(2, repository.GetAllCustomers().Count);
         }
 
         [TestMethod]
         public void GetEmptyCustomerListTest()
         {
-            repository.DeleteCustomer(1);
-            repository.DeleteCustomer(2);
-            repository.DeleteCustomer(3);
-
+            repository.GetAllCustomers().Clear();
             Exception ex = Assert.ThrowsException<Exception>(() => repository.GetAllCustomers());
             Assert.AreSame(ex.Message, "There's no customers");
         }
@@ -71,7 +74,8 @@ namespace UnitTests
         [TestMethod]
         public void GetNonExistingCustomerTest()
         {
-            Exception ex = Assert.ThrowsException<Exception>(() => repository.GetCustomerById(4));
+            repository.GetAllCustomers().Clear();
+            Exception ex = Assert.ThrowsException<Exception>(() => repository.GetCustomerById(1));
             Assert.AreSame(ex.Message, "There's no customer with such id");
         }
     }
--- task1/UnitTest/ProductRepositoryTest.cs
+++ task1/UnitTest/ProductRepositoryTest.cs
@@ -3,67 +3,70 @@ using Data.Interfaces;
 using Logic;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
+using System.Linq;
 
-namespace UnitTests
+namespace DataUnitTests
 {
     [TestClass]
     public class ProductRepositoryTest
     {
         private IProduct repository;
         private DataContext data;
+        private IDataGenerator generator;
 
         [TestInitialize]
         public void Initialize()
         {
             data = new DataContext();
             repository = new ProductRepository(data);
+            generator = new FixedExampleData();
+            //generator = new RandomExampleData();
+            generator.GenerateData(data);
         }
 
         [TestMethod]
         public void AddProductTest()
         {
-            Product product = new Product(4, "Lamp", 23.23);
-            Assert.AreEqual(3, repository.GetAllProduct().Count);
-            repository.AddProduct(product);
+            Product product = new Product(101, "Paint", 23.23);
             Assert.AreEqual(4, repository.GetAllProduct().Count);
+            repository.AddProduct(product);
+            Assert.AreEqual(5, repository.GetAllProduct().Count);
         }
 
         [TestMethod]
         public void GetProductByIdTest()
         {
-            Product product = new Product(4, "Lamp", 23.23);
+            Product product = new Product(101, "Paint", 23.23);
             repository.AddProduct(product);
-            Product productById = repository.GetProductById(4);
+            Product productById = repository.GetProductById(101);
             Assert.AreEqual(product, productById);
         }
 
         [TestMethod]
         public void UpdateProductTest()
         {
-            Product product = repository.GetProductById(1);
-            Product productUpdated = new Product(1, "Lamp", 23.23);
-            Assert.AreEqual(product, repository.GetProductById(1));
+            Product product = repository.GetAllProduct().First();
+            Product productUpdated = new Product(product.Id, "Paint", 23.23);
+            Assert.AreEqual(product, repository.GetProductById(product.Id));
             repository.UpdateProduct(productUpdated);
-            Assert.AreEqual(product, repository.GetProductById(1));
-            Assert.AreEqual(productUpdated.Name, repository.GetProductById(1).Name);
-            Assert.AreEqual(productUpdated.Price, repository.GetProductById(1).Price);
+            Assert.AreEqual(product, repository.GetProductById(product.Id));
+            Assert.AreEqual(productUpdated.Name, repository.GetProductById(product.Id).Name);
+            Assert.AreEqual(productUpdated.Price, repository.GetProductById(product.Id).Price);
         }
 
         [TestMethod]
         public void DeleteProductTest()
         {
+            Product product = repository.GetAllProduct().First();
+            Assert.AreEqual(4, repository.GetAllProduct().Count);
+            repository.DeleteProduct(product.Id);
             Assert.AreEqual(3, repository.GetAllProduct().Count);
-            repository.DeleteProduct(1);
-            Assert.AreEqual(2, repository.GetAllProduct().Count);
         }
 
         [TestMethod]
         public void GetEmptyProductListTest()
         {
-            repository.DeleteProduct(1);
-            repository.DeleteProduct(2);
-            repository.DeleteProduct(3);
-
+            repository.GetAllProduct().Clear();
             Exception ex = Assert.ThrowsException<Exception>(() => repository.GetAllProduct());
             Assert.AreSame(ex.Message, "There's no products");
         }
@@ -71,24 +74,27 @@ namespace UnitTests
         [TestMethod]
         public void GetNonExistingProductTest()
         {
-            Exception ex = Assert.ThrowsException<Exception>(() => repository.GetProductById(4));
+            repository.GetAllProduct().Clear();
+            Exception ex = Assert.ThrowsException<Exception>(() => repository.GetProductById(1));
             Assert.AreSame(ex.Message, "There's no product with such id");
         }
 
         [TestMethod]
         public void GetAvailableProductTest()
         {
+            Product product = repository.GetAllProduct().First();
+            Assert.AreEqual(4, repository.GetAvailableProducts().Count);
+            repository.ReduceAmountOfProduct(repository.GetProductById(product.Id), repository.GetProductAmountById(product.Id));
             Assert.AreEqual(3, repository.GetAvailableProducts().Count);
-            repository.ReduceAmountOfProduct(repository.GetProductById(1), repository.GetProductAmountById(1));
-            Assert.AreEqual(2, repository.GetAvailableProducts().Count);
         }
 
         [TestMethod]
         public void GetProductQtyByIdTest()
         {
-            Assert.AreEqual(11, repository.GetProductAmountById(1));
-            repository.IncreaseAmountOfProduct(repository.GetProductById(1), 11);
-            Assert.AreEqual(22, repository.GetProductAmountById(1));
+            Product product = repository.GetAllProduct().First();
+            int amount = repository.GetProductAmountById(product.Id);
+            repository.IncreaseAmountOfProduct(repository.GetProductById(product.Id), 1);
+            Assert.AreEqual(amount + 1, repository.GetProductAmountById(product.Id));
         }
     }
 }

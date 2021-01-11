3ee93c0cf2996e74182f3e6a944c4db141990120
Mateusz Stolarczyk <224102@edu.p.lodz.pl>
2021-01-11 20:28:50 +0100

    presentation project init

---
+++ task2/Presentation/ViewModel/MainViewModel.cs
@@ -0,0 +1,39 @@
+﻿using Presentation.Base;
+using Presentation.View;
+
+
+
+namespace Presentation.ViewModel
+{
+    class MainViewModel : BaseViewModel
+    {
+        public RelayCommand ToCustomerCommand { get; set; }
+        public RelayCommand ToProductCommand { get; set; }
+        public RelayCommand ToEventCommand { get; set; }
+        public MainViewModel()
+        {
+            ToCustomerCommand = new RelayCommand(ToCustomer);
+            ToProductCommand = new RelayCommand(ToProduct);
+            ToEventCommand = new RelayCommand(ToEvent);
+        }
+
+        void ToCustomer(object parameter)
+        {
+            var win = new CustomerView();
+            win.Show();
+        }
+
+        void ToProduct(object parameter)
+        {
+            var win = new ProductView();
+            win.Show();
+        }
+        void ToEvent(object parameter)
+        {
+            var win = new EventView();
+            win.Show();
+        }
+
+
+    }
+}
